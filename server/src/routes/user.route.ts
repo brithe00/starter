14af030ccb61d321e09954a7cf1ebb0c786b424e
Brian <61732049+brithe00@users.noreply.github.com>
2025-05-02 21:44:02 +0200

    updated routes to use zValidator and not the wrapper

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { UserController } from "../controllers/user.controller";
 import { z } from "zod";
-import { validate } from "../validators/validate";
+import { zValidator } from "@hono/zod-validator";
 import { createUserSchema, updateUserSchema } from "../schemas/user.schema";
 
 const userRoutes = new Hono();
@@ -12,19 +12,19 @@ const idParamSchema = z.object({
 });
 
 userRoutes.get("/", (c) => userController.getUsers(c));
-userRoutes.get("/:id", validate.params(idParamSchema), (c) =>
+userRoutes.get("/:id", zValidator("param", idParamSchema), (c) =>
   userController.getUserById(c)
 );
-userRoutes.post("/", validate.body(createUserSchema), (c) =>
+userRoutes.post("/", zValidator("json", createUserSchema), (c) =>
   userController.createUser(c)
 );
 userRoutes.put(
   "/:id",
-  validate.params(idParamSchema),
-  validate.body(updateUserSchema),
+  zValidator("param", idParamSchema),
+  zValidator("json", updateUserSchema),
   (c) => userController.updateUser(c)
 );
-userRoutes.delete("/:id", validate.params(idParamSchema), (c) =>
+userRoutes.delete("/:id", zValidator("param", idParamSchema), (c) =>
   userController.deleteUser(c)
 );
 
