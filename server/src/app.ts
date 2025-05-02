cabf0fb59d24443ade84fb4b9848ff0b199b8662
Brian <61732049+brithe00@users.noreply.github.com>
2025-05-02 21:44:30 +0200

    added middleware to attach user and session to future routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,10 +7,16 @@ import { errorMiddleware } from "./middlewares/error.middleware";
 import { env } from "./config/env";
 import { auth } from "./lib/auth";
 
-const app = new Hono();
+const app = new Hono<{
+  Variables: {
+    user: typeof auth.$Infer.Session.user | null;
+    session: typeof auth.$Infer.Session.session | null;
+  };
+}>();
 
 app.use("*", logger());
 app.use("*", prettyJSON());
+
 app.use(
   "/*",
   cors({
@@ -18,7 +24,19 @@ app.use(
     credentials: true,
   })
 );
-app.use("*", errorMiddleware);
+
+app.use("*", async (c, next) => {
+  const session = await auth.api.getSession({ headers: c.req.raw.headers });
+
+  if (session) {
+    c.set("user", session.user);
+    c.set("session", session.session);
+  } else {
+    c.set("user", null);
+    c.set("session", null);
+  }
+  return next();
+});
 
 app.on(["POST", "GET"], "/api/auth/**", (c) => auth.handler(c.req.raw));
 
@@ -28,4 +46,7 @@ app.get("/", (c) => c.json({ message: "API is running" }));
 
 app.notFound((c) => c.json({ message: "Not Found" }, 404));
 
+app.use("*", errorMiddleware);
+
 export default app;
+export type AppType = typeof app;
